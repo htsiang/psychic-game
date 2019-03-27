1316e260bffd168bb47c7525fec3661c65d40ebb
htsiang <htsiang3@gmail.com>
2019-03-26 20:50:27 -0700

    Fixed error where computerLetter variable was being rewritten to undefined w/ each key press, thus making it impossible to equal userGuess & making the game unwinnable.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,18 +3,16 @@ var letterArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "
 var numberOfWins = 0;
 var numberOfLosses = 0;
 var numberOfGuesses = 9;
+var computerLetter = "";
 
 
 // functin runs whenever user presses a key
 document.onkeyup = function() {
 
-    // Computer pseudo randomly chooses letter from letterArray
+    // Computer pseudo randomly chooses letter from letterArray to place in computerLetter
     //but only when numberOfGuess===9
-    if(numberOfGuesses===9) {
-        var computerLetter = letterArray[Math.floor(Math.random()*letterArray.length)];
-        console.log("computer letter: " + computerLetter);
-        console.log(typeof computerLetter);
-        console.log(computerLetter.length);
+    if (numberOfGuesses===9) {
+        computerLetter = letterArray[Math.floor(Math.random()*letterArray.length)];
     };
 
     // decreases # of guesses left & display new amount
@@ -25,16 +23,14 @@ document.onkeyup = function() {
     //determines which key was pressed
     var userGuess = event.key;
     userGuess = userGuess.toLowerCase();
-    console.log(userGuess);
-    console.log(typeof userGuess);
-    console.log(userGuess.length);
+    
 
     // get the element from html doc - guesses
     var guesses = document.getElementById("guesses");
     //dynamically create new element
-    var newGuess = document.createElement("div");
+    var newGuess = document.createElement("span");
     // places userGuess on html document using newGuess element
-    newGuess.textContent = userGuess;
+    newGuess.textContent = userGuess + ", ";
     guesses.appendChild(newGuess);
 
     // if no guesses remaining, reset & increment # of losses
@@ -48,8 +44,16 @@ document.onkeyup = function() {
         guessRemainder.textContent = "Guesses Left: 9";
     };
 
+    console.log("computer letter: " + computerLetter);
+    console.log(typeof computerLetter);
+    console.log(computerLetter.length);
+
+    console.log(userGuess);
+    console.log(typeof userGuess);
+    console.log(userGuess.length);
+
     // if userGuess is correct, reset & increment # of wins
-    if(computerLetter===userGuess) {
+    if (computerLetter===userGuess) {
         numberOfWins++;
 
         var winCount = document.getElementById("winCount");
@@ -57,6 +61,7 @@ document.onkeyup = function() {
 
         numberOfGuesses=9;
         guesses.textContent = "Your Guesses so far: ";
+        guessRemainder.textContent = "Guesses Left: 9";
     };
 
 };
